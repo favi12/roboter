94fdb1de24d47eeaae29aa8a4db4755e8ba6bcad
favian <fabian.lozano@hotmail.de>
2025-05-28 18:29:10 +0200

    ja

--- Roboter.java
+++ Roboter.java
@@ -1,7 +1,34 @@
 Public Class Roboter.java {
     private String name;
-    
+    private String Batterie;
+    private int distance;
+    //konstruktor wird hier definiert!
+    public Roboter(String name, String Batterie; int distance) {
+        this.name = name;
+        this.Batterie = 100;
+        this.distance = 0;
+    }
 
+    public String getname() {
+        return name;
+    }
+
+    public String getBatterie() {
+        return Batterie;
+    }
+
+    public int getdistance() {
+        get distance;
+    }
+
+    @Override 
+    public String toString() {
+        return "{ +
+                " name=' " + name + "'" 
+        
+        
+        }"
+    }
 
 
 
--- Run.java
+++ Run.java
@@ -1,3 +1,6 @@
 public class Run {
-    
+    public static void main(String[] args) {
+        roboter alpha = new Roboter(name:"alpha");
+        roboter beta = new Roboter(name:"beta");
+    }
 }
